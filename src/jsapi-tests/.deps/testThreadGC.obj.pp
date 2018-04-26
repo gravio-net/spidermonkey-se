df6f54dfe28fa4ce50aa32cb3022e3b3290ec4d9
demuskov <demuskov@gmail.com>
2018-04-26 13:38:53 +0300

    Initial public release

---
+++ src/jsapi-tests/.deps/testThreadGC.obj.pp
@@ -0,0 +1,1 @@
+testThreadGC.obj: c:\projects\jet.framework.rc5\3dparty\js\src\jsapi-tests\../js-confdefs.h
---
+++ src/jsapi-tests/.deps/testThreads.obj.pp
@@ -0,0 +1,1 @@
+testThreads.obj: c:\projects\jet.framework.rc5\3dparty\js\src\jsapi-tests\../js-confdefs.h
